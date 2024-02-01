41251753858d18d5cb710ccc0addd391ea2de718
Arun Prasath <136972301+Arun-foxy@users.noreply.github.com>
2024-02-01 23:51:05 +0530

    Update frist-script.py
    changes done

--- IAM-user/frist-script.py
+++ IAM-user/frist-script.py
@@ -1,1 +1,8 @@
+import boto3
 
+aws_management_console = boto3.session.Session(profile_name="default")
+iam_console = aws_management_console.resource('iam') # here we can give any services name 'iam,ec2',
+
+
+for each_user in iam_console.users.all():
+    print(each_user.name)
